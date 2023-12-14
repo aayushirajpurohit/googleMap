f42a58a3544b643fa9a543cd98f4a88df49fa3ff
aayushirajpurohit <ayushi.rajpurohit@siplmtuy.local>
2023-12-14 11:52:14 +0530

    only text render

--- App.tsx
+++ App.tsx
@@ -1,3 +1,67 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * Generated with the TypeScript template
+//  * https://github.com/react-native-community/react-native-template-typescript
+//  *
+//  * @format
+//  */
+
+// import { NavigationContainer } from "@react-navigation/native";
+// import React, { type PropsWithChildren, useEffect } from "react";
+// import {
+//   Dimensions,
+//   Image,
+//   Platform,
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+// } from "react-native";
+// import { Colors } from "react-native/Libraries/NewAppScreen";
+// import NavigationStack from "./src/navigation/NavigationStack";
+// import {enableLatestRenderer} from 'react-native-maps';
+
+
+
+
+// const App = () => {
+//   enableLatestRenderer();
+//   const isDarkMode = useColorScheme() === "dark";
+
+
+
+
+//   const { height, width } = Dimensions.get("window");
+
+//   return (
+//     <View style={[{ flex: 1 }]}>
+     
+//       <View style={styles.container}>
+     
+//         <NavigationContainer>
+//           <NavigationStack />
+//         </NavigationContainer>
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: "column",
+//   },
+// });
+
+// export default App;
+
+
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -9,7 +73,7 @@
  */
 
 import { NavigationContainer } from "@react-navigation/native";
-import React, { type PropsWithChildren, useEffect } from "react";
+import React, {  useEffect } from "react";
 import {
   Dimensions,
   Image,
@@ -23,31 +87,19 @@ import {
   View,
 } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
-import NavigationStack from "./src/navigation/NavigationStack";
-import {enableLatestRenderer} from 'react-native-maps';
-
-
 
 
 const App = () => {
-  enableLatestRenderer();
-  const isDarkMode = useColorScheme() === "dark";
-
-
-
 
   const { height, width } = Dimensions.get("window");
 
   return (
-    <View style={[{ flex: 1 }]}>
+    <SafeAreaView style={[{ flex: 1 }]}>
      
       <View style={styles.container}>
-     
-        <NavigationContainer>
-          <NavigationStack />
-        </NavigationContainer>
+<Text style={{ color:'red',fontSize:25}}>Hello!!!!!</Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -55,7 +107,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
+    justifyContent:'center',
+    alignItems:'center',
+   
   },
 });
 
 export default App;
+
+
+
